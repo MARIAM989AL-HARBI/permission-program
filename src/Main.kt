858ee10a08409746efe9d99eb5b6ed9b349528a4
Mariam AL-harbi <mariamam205@hotmail.com>
2021-11-21 22:15:08 +0300

    Initial commit

---
+++ src/Main.kt
@@ -0,0 +1,11 @@
+fun main() {
+
+   val num = readLine()!!.toInt()
+   if (num <= 18) {
+      println("You are not old enough...")
+
+   } else {
+      println("welcome!!!!!!")
+   }
+}
+
---
+++ src/text.kt
@@ -0,0 +1,12 @@
+fun main(){
+
+    val num = readLine()!!.toInt()
+    if(num <=18){
+        println("you are not old enough....")
+
+
+    }else{
+        println("welcome!!!")
+    }
+
+}
